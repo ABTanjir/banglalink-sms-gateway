8904c2fcebe12b2beea4a16eb391fb3b7d261ed3
ABTanjir <ABTanjir@users.noreply.github.com>
2018-12-11 05:10:14 +0600

    Create banglalink.php

---
+++ config/banglalink.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * everything you declare in config array
+ * you can easily access it from the Banglalink library with an underscore
+ * for example if you create $config['variable'] you can access it from library by simply calling $this->_variable
+ */
+
+#page access token
+$config['api']	= "https://vas.banglalinkgsm.com";
+$config['username']	= "your username";
+$config['password']	= "your password";
+$config['sender']	= "your masking"; 
+  
